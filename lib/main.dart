c7815b597de9d961080ee4213628f89ed17d1b41
u2-vertice <u2.bitbucket@gmail.com>
2019-04-30 17:19:05 +0700

    loging in with the main & user model

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,7 @@ import './pages/auth.dart';
 import './pages/products.dart';
 import './pages/product.dart';
 import './pages/products_admin.dart';
-import './models/product.dart';
-import './scoped-models/products.dart';
+import './scoped-models/main.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,8 +22,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return ScopedModel<ProductsModel>(
-        model: ProductsModel(),
+    return ScopedModel<MainModel>(
+        model: MainModel(),
         child: MaterialApp(
           theme: ThemeData(
               brightness: Brightness.light,
--- lib/pages/product_edit.dart
+++ lib/pages/product_edit.dart
@@ -4,7 +4,7 @@ import 'package:scoped_model/scoped_model.dart';
 
 import '../widgets/helpers/ensure-visible.dart';
 import '../models/product.dart';
-import '../scoped-models/products.dart';
+import '../scoped-models/main.dart';
 
 class ProductEditPage extends StatefulWidget {
   @override
@@ -86,8 +86,8 @@ class _ProductEditPageState extends State<ProductEditPage> {
   }
 
   Widget _buildSubmitButton() {
-    return ScopedModelDescendant<ProductsModel>(
-      builder: (BuildContext context, Widget widget, ProductsModel model) {
+    return ScopedModelDescendant<MainModel>(
+      builder: (BuildContext context, Widget widget, MainModel model) {
         return RaisedButton(
           child: Text('Save'),
           textColor: Colors.white,
@@ -156,8 +156,8 @@ class _ProductEditPageState extends State<ProductEditPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ScopedModelDescendant<ProductsModel>(
-      builder: (BuildContext context, Widget widget, ProductsModel model) {
+    return ScopedModelDescendant<MainModel>(
+      builder: (BuildContext context, Widget widget, MainModel model) {
         final Widget pageContent =
             _buildPageContent(context, model.selectedProduct);
         return model.selectedProductIndex == null
